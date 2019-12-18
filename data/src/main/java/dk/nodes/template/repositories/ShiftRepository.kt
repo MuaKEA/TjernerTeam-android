c20d09ca0061d854555006cc864a91fb999434bd
marcuselmgreen <marcus.elmgreen@hotmail.com>
2019-12-18 11:50:47 +0100

    Removed Logs from repository

--- data/src/main/java/dk/nodes/template/repositories/ShiftRepository.kt
+++ data/src/main/java/dk/nodes/template/repositories/ShiftRepository.kt
@@ -1,6 +1,5 @@
 package dk.nodes.template.repositories
 
-import android.util.Log
 import com.google.gson.Gson
 import dk.nodes.template.models.Shift
 import dk.nodes.template.network.ShiftService
@@ -13,24 +12,15 @@ class ShiftRepository @Inject constructor(
 
    suspend fun getShifts(): ArrayList<Shift> {
         val shiftsArrayList =ArrayList<Shift>()
-        Log.d("tupac", "shakur")
-
         val response = api.getActiveShifts().execute()
-
-        Log.d("tupac", response.body().toString())
-
-
 
         if (response.isSuccessful) {
            val shiftResponse = response.body()
-
             if (shiftResponse != null){
                 shiftsArrayList.addAll(shiftResponse.shiftsWrapper)
-                Log.d("tupac", "shakur3")
                 return shiftsArrayList
             }
         }
-
         return shiftsArrayList
     }
 }
